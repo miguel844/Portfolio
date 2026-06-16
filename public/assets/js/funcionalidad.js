b8e7433d6afb74b6a6e20716a647397c9d71d341
Miguel Angel Cabello Mesa <miguel844@gmail.com>
2026-06-16 21:41:21 +0200

    Separamos el JavaScript del html

--- public/assets/js/funcionalidad.js
+++ public/assets/js/funcionalidad.js
@@ -2,6 +2,8 @@
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("mobileMenu");
 
+console.log(menuBtn);
+console.log(menu);
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
