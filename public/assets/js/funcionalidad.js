6fbbeb46cac0afd216f3bc329682620c420c1386
Miguel Angel Cabello Mesa <miguel844@gmail.com>
2026-06-20 19:42:15 +0200

    Nuevo diseño de portfolio

--- public/assets/js/funcionalidad.js
+++ public/assets/js/funcionalidad.js
@@ -1,7 +1,45 @@
-// boton menu hamburguesa
-const menuBtn = document.getElementById("menuBtn");
-const menu = document.getElementById("mobileMenu");
+const skill = ["HTML","CSS","Tailwind","JavaScript","PHP","Laravel","MySQL"];
+let salidaSkill = document.getElementById("salidaSkill");
 
-menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
+// Creamos función para pintar skills
+function pintarSkills() {
+    skill.forEach(habilidad => {
+        let nuevoDiv = document.createElement("div");
+
+        nuevoDiv.classList.add(
+            "font-titulo",
+            "text-3xl",
+            "p-6",
+            "md:px-20",
+        );
+
+        nuevoDiv.textContent = habilidad;
+        salidaSkill.appendChild(nuevoDiv);
+    });
+}
+
+pintarSkills();
+pintarSkills();
+
+
+let posicion = 0;
+
+function mover() {
+    posicion -= 0.5; // velocidad (más alto = más rápido)
+
+    salidaSkill.style.transform = `translateX(${posicion}px)`;
+
+    // reinicio suave cuando llega a mitad
+    if (Math.abs(posicion) >= salidaSkill.scrollWidth / 2) {
+        posicion = 0;
+    }
+
+    requestAnimationFrame(mover);
+}
+
+mover();
+
+function crearProyecto(){
+
+
+}
--- public/assets/js/gsap.js
+++ public/assets/js/gsap.js
@@ -1,3 +1,4 @@
+/*
 // Logo
 gsap.from(".logo",{
     x:-200,
@@ -19,5 +20,24 @@ gsap.from(".menu-item",{
     stagger:0.1
 });
 
-
+*/
 /* https://www.youtube.com/watch?v=66rpLEBmYP4 */
+
+
+const flecha = document.querySelector(".flecha");
+
+flecha.addEventListener("mouseenter", () => {
+    gsap.to(flecha, {
+        x: 16,
+        duration: 0.3,
+        color: "white"
+    });
+});
+
+flecha.addEventListener("mouseleave", () => {
+    gsap.to(flecha, {
+        x: 0,
+        duration: 0.3,
+        color: "#ff5050"
+    });
+});
