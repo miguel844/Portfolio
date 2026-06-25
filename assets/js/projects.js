6749edcb25fec7f68836c86213805bbe5034b0ff
Miguel Angel Cabello Mesa <miguel844@gmail.com>
2026-06-25 20:19:18 +0200

    resize nav and footer

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -20,7 +20,7 @@ const proyectos = [
     titulo: "Portfolio",
     descripcion: "Portfolio personal responsive desarrollado con Tailwind.  lorem asdasda sdasdasd asdasd asdasdasd asdasd asdasc xasdas d asdasd",
     imagen: "/portfolio/public/assets/img/portfolio.png",
-    tecnologias: ["HTML", "Tailwind", "GSAP"],
+    tecnologias: ["HTML", "Tailwind", "JavaScript"],
     github: "https://github.com/usuario/portfolio",
     demo: "https://demo-portfolio.com",
     mostrarPaginaPrincipal: false
